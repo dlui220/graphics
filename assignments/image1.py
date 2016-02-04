df48baf59fb8a137eea90a41154ab0de1e008929
Derrick Lui <derricklui1@gmail.com>
2016-02-04 14:47:51 -0500

    updated

---
+++ assignments/image1.py
@@ -0,0 +1,22 @@
+def createImage(xres,yres):
+    switch = 1
+    image = open("image1.ppm",'w')
+    s = "P3 " + str(xres) + " " + str(yres) + " 255 "
+    r = 255
+    g = 0
+    b = 255
+    for i in range(yres):
+        for j in range(xres):
+            s += str(r) + " " + str(g) + " " + str(b) + " "
+        r -= 1 * switch
+        g += 1 * switch
+        b -= 1 * switch
+        if (g == 255):
+            switch = switch * -1
+        if (switch == -1 and g == 0):
+            switch = switch * -1
+
+    image.write(s)
+    image.close()
+
+createImage(1000,1000)
